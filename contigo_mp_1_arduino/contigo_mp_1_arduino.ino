3e6e2f792a3b95f27314e278150cc21b5a546f2a
Mok <yunyoungmok@gmail.com>
2021-08-16 22:33:39 -0500

    long arduino file has been split for better readability and maintenance

--- contigo_mp_1_arduino/contigo_mp_1_arduino.ino
+++ contigo_mp_1_arduino/contigo_mp_1_arduino.ino
@@ -1,372 +1,29 @@
-#define ID1   1
-#define ID2   2
+#include "lobot_servo.h"
 
-
-
-#define GET_LOW_BYTE(A) (uint8_t)((A))
-//宏函数 获得A的低八位
-#define GET_HIGH_BYTE(A) (uint8_t)((A) >> 8)
-//宏函数 获得A的高八位
-#define BYTE_TO_HW(A, B) ((((uint16_t)(A)) << 8) | (uint8_t)(B))
-//宏函数 以A为高八位 B为低八位 合并为16位整形
-
-#define LOBOT_SERVO_FRAME_HEADER         0x55
-#define LOBOT_SERVO_MOVE_TIME_WRITE      1
-#define LOBOT_SERVO_MOVE_TIME_READ       2
-#define LOBOT_SERVO_MOVE_TIME_WAIT_WRITE 7
-#define LOBOT_SERVO_MOVE_TIME_WAIT_READ  8
-#define LOBOT_SERVO_MOVE_START           11
-#define LOBOT_SERVO_MOVE_STOP            12
-#define LOBOT_SERVO_ID_WRITE             13
-#define LOBOT_SERVO_ID_READ              14
-#define LOBOT_SERVO_ANGLE_OFFSET_ADJUST  17
-#define LOBOT_SERVO_ANGLE_OFFSET_WRITE   18
-#define LOBOT_SERVO_ANGLE_OFFSET_READ    19
-#define LOBOT_SERVO_ANGLE_LIMIT_WRITE    20
-#define LOBOT_SERVO_ANGLE_LIMIT_READ     21
-#define LOBOT_SERVO_VIN_LIMIT_WRITE      22
-#define LOBOT_SERVO_VIN_LIMIT_READ       23
-#define LOBOT_SERVO_TEMP_MAX_LIMIT_WRITE 24
-#define LOBOT_SERVO_TEMP_MAX_LIMIT_READ  25
-#define LOBOT_SERVO_TEMP_READ            26
-#define LOBOT_SERVO_VIN_READ             27
-#define LOBOT_SERVO_POS_READ             28
-#define LOBOT_SERVO_OR_MOTOR_MODE_WRITE  29
-#define LOBOT_SERVO_OR_MOTOR_MODE_READ   30
-#define LOBOT_SERVO_LOAD_OR_UNLOAD_WRITE 31
-#define LOBOT_SERVO_LOAD_OR_UNLOAD_READ  32
-#define LOBOT_SERVO_LED_CTRL_WRITE       33
-#define LOBOT_SERVO_LED_CTRL_READ        34
-#define LOBOT_SERVO_LED_ERROR_WRITE      35
-#define LOBOT_SERVO_LED_ERROR_READ       36
-
-#define LOBOT_DEBUG 1  /*调试是用，打印调试数据*/
-
-byte LobotCheckSum(byte buf[])
-{
-  byte i;
-  uint16_t temp = 0;
-  for (i = 2; i < buf[3] + 2; i++) {
-    temp += buf[i];
-  }
-  temp = ~temp;
-  i = (byte)temp;
-  return i;
-}
-
-void LobotSerialServoMove(HardwareSerial &SerialX, uint8_t id, int16_t position, uint16_t time)
-{
-  byte buf[10];
-  if(position < 0)
-    position = 0;
-  if(position > 1000)
-    position = 1000;
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = id;
-  buf[3] = 7;
-  buf[4] = LOBOT_SERVO_MOVE_TIME_WRITE;
-  buf[5] = GET_LOW_BYTE(position);
-  buf[6] = GET_HIGH_BYTE(position);
-  buf[7] = GET_LOW_BYTE(time);
-  buf[8] = GET_HIGH_BYTE(time);
-  buf[9] = LobotCheckSum(buf);
-  SerialX.write(buf, 10);
-}
-
-void LobotSerialServoStopMove(HardwareSerial &SerialX, uint8_t id)
-{
-  byte buf[6];
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = id;
-  buf[3] = 3;
-  buf[4] = LOBOT_SERVO_MOVE_STOP;
-  buf[5] = LobotCheckSum(buf);
-  SerialX.write(buf, 6);
-}
-
-void LobotSerialServoSetID(HardwareSerial &SerialX, uint8_t oldID, uint8_t newID)
-{
-  byte buf[7];
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = oldID;
-  buf[3] = 4;
-  buf[4] = LOBOT_SERVO_ID_WRITE;
-  buf[5] = newID;
-  buf[6] = LobotCheckSum(buf);
-  SerialX.write(buf, 7);
-  
-#ifdef LOBOT_DEBUG
-  Serial.println("LOBOT SERVO ID WRITE");
-  int debug_value_i = 0;
-  for (debug_value_i = 0; debug_value_i < buf[3] + 3; debug_value_i++)
-  {
-    Serial.print(buf[debug_value_i], HEX);
-    Serial.print(":");
-  }
-  Serial.println(" ");
-#endif
-
-}
-
-void LobotSerialServoSetMode(HardwareSerial &SerialX, uint8_t id, uint8_t Mode, int16_t Speed)
-{
-  byte buf[10];
-
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = id;
-  buf[3] = 7;
-  buf[4] = LOBOT_SERVO_OR_MOTOR_MODE_WRITE;
-  buf[5] = Mode;
-  buf[6] = 0;
-  buf[7] = GET_LOW_BYTE((uint16_t)Speed);
-  buf[8] = GET_HIGH_BYTE((uint16_t)Speed);
-  buf[9] = LobotCheckSum(buf);
-
-#ifdef LOBOT_DEBUG
-  Serial.println("LOBOT SERVO Set Mode");
-  int debug_value_i = 0;
-  for (debug_value_i = 0; debug_value_i < buf[3] + 3; debug_value_i++)
-  {
-    Serial.print(buf[debug_value_i], HEX);
-    Serial.print(":");
-  }
-  Serial.println(" ");
-#endif
-
-  SerialX.write(buf, 10);
-}
-void LobotSerialServoLoad(HardwareSerial &SerialX, uint8_t id)
-{
-  byte buf[7];
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = id;
-  buf[3] = 4;
-  buf[4] = LOBOT_SERVO_LOAD_OR_UNLOAD_WRITE;
-  buf[5] = 1;
-  buf[6] = LobotCheckSum(buf);
-  
-  SerialX.write(buf, 7);
-  
-#ifdef LOBOT_DEBUG
-  Serial.println("LOBOT SERVO LOAD WRITE");
-  int debug_value_i = 0;
-  for (debug_value_i = 0; debug_value_i < buf[3] + 3; debug_value_i++)
-  {
-    Serial.print(buf[debug_value_i], HEX);
-    Serial.print(":");
-  }
-  Serial.println(" ");
-#endif
-
-}
-
-void LobotSerialServoUnload(HardwareSerial &SerialX, uint8_t id)
-{
-  byte buf[7];
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = id;
-  buf[3] = 4;
-  buf[4] = LOBOT_SERVO_LOAD_OR_UNLOAD_WRITE;
-  buf[5] = 0;
-  buf[6] = LobotCheckSum(buf);
-  
-  SerialX.write(buf, 7);
-  
-#ifdef LOBOT_DEBUG
-  Serial.println("LOBOT SERVO LOAD WRITE");
-  int debug_value_i = 0;
-  for (debug_value_i = 0; debug_value_i < buf[3] + 3; debug_value_i++)
-  {
-    Serial.print(buf[debug_value_i], HEX);
-    Serial.print(":");
-  }
-  Serial.println(" ");
-#endif
-}
-
-
-int LobotSerialServoReceiveHandle(HardwareSerial &SerialX, byte *ret)
-{
-  bool frameStarted = false;
-  bool receiveFinished = false;
-  byte frameCount = 0;
-  byte dataCount = 0;
-  byte dataLength = 2;
-  byte rxBuf;
-  byte recvBuf[32];
-  byte i;
-
-  while (SerialX.available()) {
-    rxBuf = SerialX.read();
-    delayMicroseconds(100);
-    if (!frameStarted) {
-      if (rxBuf == LOBOT_SERVO_FRAME_HEADER) {
-        frameCount++;
-        if (frameCount == 2) {
-          frameCount = 0;
-          frameStarted = true;
-          dataCount = 1;
-        }
-      }
-      else {
-        frameStarted = false;
-        dataCount = 0;
-        frameCount = 0;
-      }
-    }
-    if (frameStarted) {
-      recvBuf[dataCount] = (uint8_t)rxBuf;
-      if (dataCount == 3) {
-        dataLength = recvBuf[dataCount];
-        if (dataLength < 3 || dataCount > 7) {
-          dataLength = 2;
-          frameStarted = false;
-        }
-      }
-      dataCount++;
-      if (dataCount == dataLength + 3) {
-        
-#ifdef LOBOT_DEBUG
-        Serial.print("RECEIVE DATA:");
-        for (i = 0; i < dataCount; i++) {
-          Serial.print(recvBuf[i], HEX);
-          Serial.print(":");
-        }
-        Serial.println(" ");
-#endif
-
-        if (LobotCheckSum(recvBuf) == recvBuf[dataCount - 1]) {
-          
-#ifdef LOBOT_DEBUG
-          Serial.println("Check SUM OK!!");
-          Serial.println("");
-#endif
-
-          frameStarted = false;
-          memcpy(ret, recvBuf + 4, dataLength);
-          return 1;
-        }
-        return -1;
-      }
-    }
-  }
-}
-
-
-int LobotSerialServoReadPosition(HardwareSerial &SerialX, uint8_t id)
-{
-  int count = 10000;
-  int ret;
-  byte buf[6];
-
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = id;
-  buf[3] = 3;
-  buf[4] = LOBOT_SERVO_POS_READ;
-  buf[5] = LobotCheckSum(buf);
-
-#ifdef LOBOT_DEBUG
-  Serial.println("LOBOT SERVO Pos READ");
-  int debug_value_i = 0;
-  for (debug_value_i = 0; debug_value_i < buf[3] + 3; debug_value_i++)
-  {
-    Serial.print(buf[debug_value_i], HEX);
-    Serial.print(":");
-  }
-  Serial.println(" ");
-#endif
-
-  while (SerialX.available())
-    SerialX.read();
-
-  SerialX.write(buf, 6);
-
-  while (!SerialX.available()) {
-    count -= 1;
-    if (count < 0)
-      return -2048;
-  }
-
-  if (LobotSerialServoReceiveHandle(SerialX, buf) > 0)
-    ret = (int16_t)BYTE_TO_HW(buf[2], buf[1]);
-  else
-    ret = -2048;
-
-#ifdef LOBOT_DEBUG
-  Serial.println(ret);
-#endif
-  return ret;
-}
-int LobotSerialServoReadVin(HardwareSerial &SerialX, uint8_t id)
-{
-  int count = 10000;
-  int ret;
-  byte buf[6];
-
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = id;
-  buf[3] = 3;
-  buf[4] = LOBOT_SERVO_VIN_READ;
-  buf[5] = LobotCheckSum(buf);
-
-#ifdef LOBOT_DEBUG
-  Serial.println("LOBOT SERVO VIN READ");
-  int debug_value_i = 0;
-  for (debug_value_i = 0; debug_value_i < buf[3] + 3; debug_value_i++)
-  {
-    Serial.print(buf[debug_value_i], HEX);
-    Serial.print(":");
-  }
-  Serial.println(" ");
-#endif
-
-  while (SerialX.available())
-    SerialX.read();
-
-  SerialX.write(buf, 6);
-
-  while (!SerialX.available()) {
-    count -= 1;
-    if (count < 0)
-      return -2048;
-  }
-
-  if (LobotSerialServoReceiveHandle(SerialX, buf) > 0)
-    ret = (int16_t)BYTE_TO_HW(buf[2], buf[1]);
-  else
-    ret = -2049;
-
-#ifdef LOBOT_DEBUG
-  Serial.println(ret);
-#endif
-  return ret;
-}
-
-int m1_d = 23;
-int m1_w = 3;
-int m2_d = 22;
-int m2_w = 2;
+int m1_dir = 23;
+int m1_pwm = 3;
+int m2_dir = 22;
+int m2_pwm = 2;
 
 int incomingByte =5;
 int left_v = 0;
 int right_v = 0;
-
 
 void setup() {
   // lobot servo motor to control the pan tilt of tablet
   Serial1.begin(115200);
 
   // pin mode setup
-  pinMode(m1_d, OUTPUT);
-  pinMode(m1_w, OUTPUT);
-  pinMode(m2_d, OUTPUT);
-  pinMode(m2_w, OUTPUT);
+  pinMode(m1_dir, OUTPUT);
+  pinMode(m1_pwm, OUTPUT);
+  pinMode(m2_dir, OUTPUT);
+  pinMode(m2_pwm, OUTPUT);
 
   // init of the pin outputs
-  digitalWrite(m1_d, LOW);
-  digitalWrite(m1_w, LOW);
-  digitalWrite(m2_d, LOW);
-  digitalWrite(m2_w, LOW);  
+  digitalWrite(m1_dir, LOW);
+  digitalWrite(m1_pwm, LOW);
+  digitalWrite(m2_dir, LOW);
+  digitalWrite(m2_pwm, LOW);  
 
   Serial.begin(57600);           // set up Serial library at 9600 bps
   Serial.println("Hello Arduino Code is starting");  // prints hello with ending line break 
@@ -376,13 +33,13 @@ void RightWheelVel(int v)  // v needs to be between -255 to 255
 {
   if (v>0)
   {
-    digitalWrite(m2_d, HIGH); 
-    analogWrite(m2_w, v);
+    digitalWrite(m2_dir, HIGH); 
+    analogWrite(m2_pwm, v);
   }
   else
   {
-    digitalWrite(m2_d, LOW); 
-    analogWrite(m2_w, -v);
+    digitalWrite(m2_dir, LOW); 
+    analogWrite(m2_pwm, -v);
   }
 }
 
@@ -390,13 +47,13 @@ void LeftWheelVel(int v)  // v needs to be between -255 to 255
 {
   if (v>0)
   {
-    digitalWrite(m1_d, HIGH); 
-    analogWrite(m1_w, v);
+    digitalWrite(m1_dir, HIGH); 
+    analogWrite(m1_pwm, v);
   }
   else
   {
-    digitalWrite(m1_d, LOW); 
-    analogWrite(m1_w, -v);
+    digitalWrite(m1_dir, LOW); 
+    analogWrite(m1_pwm, -v);
   }
 }
 
@@ -408,6 +65,7 @@ void WheelVel(int right_v, int left_v)
 
 int pan_ang = 500;
 int tilt_ang = 600;
+int stepsize_tablet_mount_movement = 1;
 
 void loop() {
   int i = 0;
@@ -421,34 +79,34 @@ void loop() {
   switch (cmd_tab_mount)
   {
     case 7:
-      pan_ang = pan_ang - 1;
-      tilt_ang = tilt_ang + 1;
+      pan_ang = pan_ang - stepsize_tablet_mount_movement;
+      tilt_ang = tilt_ang + stepsize_tablet_mount_movement;
       break;
     case 8:
-      tilt_ang = tilt_ang + 1;
+      tilt_ang = tilt_ang + stepsize_tablet_mount_movement;
       break;
     case 9:
-      pan_ang = pan_ang + 1;
-      tilt_ang = tilt_ang + 1;
+      pan_ang = pan_ang + stepsize_tablet_mount_movement;
+      tilt_ang = tilt_ang + stepsize_tablet_mount_movement;
       break;
     case 4:
-      pan_ang = pan_ang - 1;
+      pan_ang = pan_ang - stepsize_tablet_mount_movement;
       break;
     case 5:
       break;
     case 6:
-      pan_ang = pan_ang + 1;
+      pan_ang = pan_ang + stepsize_tablet_mount_movement;
       break;
     case 1:
-      pan_ang = pan_ang - 1;
-      tilt_ang = tilt_ang - 1;
+      pan_ang = pan_ang - stepsize_tablet_mount_movement;
+      tilt_ang = tilt_ang - stepsize_tablet_mount_movement;
       break;
     case 2:
-      tilt_ang = tilt_ang - 1;
+      tilt_ang = tilt_ang - stepsize_tablet_mount_movement;
       break;
     case 3:
-      pan_ang = pan_ang + 1;
-      tilt_ang = tilt_ang - 1;
+      pan_ang = pan_ang + stepsize_tablet_mount_movement;
+      tilt_ang = tilt_ang - stepsize_tablet_mount_movement;
       break;
     case 0:
       pan_ang = 500;
